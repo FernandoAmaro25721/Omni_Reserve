787b0f10266f5929e9492152d0f7f8a93c542928
FernandoAmaro25721 <anferama2018@gmail.com>
2026-09-16 21:03:12 -0600

    feat(tarea2): conectar AddApplication y AddInfrastructure en Program.cs

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -1,20 +1,24 @@
+using OmniReserve.Application;
+using OmniReserve.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Agregar el servicio generador de Swagger (DI)
+// Registrar servicios de las capas
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 2. Agregar el Middleware en el Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// 3. Middlewares restantes
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
--- OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
+++ OmniReserve.Application/obj/Debug/net10.0/OmniReserve.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4910ecc143d81784e16d46d214b58509d08b39da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7534ca37c45945985c975b8c032d6dd1a314e398")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
