46299cefface6ae5e2b1f8ad4005f0865bb61536
FernandoAmaro25721 <anferama2018@gmail.com>
2026-09-16 20:22:42 -0600

    Tarea 1: Configuración de Swagger UI en OmniReserve.Api

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -1,21 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. Registrar servicios en el contenedor de Inyección de Dependencias
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. Configurar el pipeline de solicitudes HTTP (Middleware)
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OmniReserve.Api v1");
+    });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
