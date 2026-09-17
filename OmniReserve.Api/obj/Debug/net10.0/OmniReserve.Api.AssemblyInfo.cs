68d19016a64c993df78631207f7bd226ad2a0ce6
FernandoAmaro25721 <anferama2018@gmail.com>
2026-09-16 21:37:56 -0600

    refactor(actividad3): crear entidades User, Room y Reservation con encapsulamiento y manejo de errores correctamente agregados

--- OmniReserve.Api/obj/Debug/net10.0/OmniReserve.Api.AssemblyInfo.cs
+++ OmniReserve.Api/obj/Debug/net10.0/OmniReserve.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4910ecc143d81784e16d46d214b58509d08b39da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef37cc239916995b48b5e18d84fbf720489db6c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
