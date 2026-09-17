9f43ce7e339aba156a764802c73daed3eb2d48f7
FernandoAmaro25721 <anferama2018@gmail.com>
2026-09-16 22:19:49 -0600

    refactor(actividad4): refactorizar CreateRoomCommandHandler para usar Task.FromResult segun la solucion del maestro

--- OmniReserve.Domain/obj/Debug/net10.0/OmniReserve.Domain.AssemblyInfo.cs
+++ OmniReserve.Domain/obj/Debug/net10.0/OmniReserve.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OmniReserve.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+172492da3dc4fa34498c6484db3909f7ae3a6973")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23f7b975cb718f09de61f0a3a04781922d6cd99")]
 [assembly: System.Reflection.AssemblyProductAttribute("OmniReserve.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OmniReserve.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
