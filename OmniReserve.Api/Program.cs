34244fb506db4700fef76a0e7f3a9755a76f1892
FernandoAmaro25721 <anferama2018@gmail.com>
2026-09-16 20:33:37 -0600

    refactor(tarea1): aplique la solucion del maestro para el Swagger UI y simplifique el middleware

--- OmniReserve.Api/Program.cs
+++ OmniReserve.Api/Program.cs
@@ -1,25 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Registrar servicios en el contenedor de Inyección de Dependencias
+// 1. Agregar el servicio generador de Swagger (DI)
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 2. Configurar el pipeline de solicitudes HTTP (Middleware)
+// 2. Agregar el Middleware en el Pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "OmniReserve.Api v1");
-    });
+    app.UseSwaggerUI();
 }
 
+// 3. Middlewares restantes
 app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
